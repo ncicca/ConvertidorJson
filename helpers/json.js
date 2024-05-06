ce6756c1277cbe00f1f9d42586d5043a0fd54372
ncicca <168201721+ncicca@users.noreply.github.com>
2024-05-06 17:07:56 -0300

    getpostClientes y json

--- helpers/json.js
+++ helpers/json.js
@@ -1,21 +1,16 @@
-// npm install: xlsx, fs, moment & npm init -y
 const xlsx = require('xlsx');
 const fs = require('fs');
 const moment = require('moment');
-const axios = require('axios');
-
-
-const excelFilePath = 'C:/Users/admin/Desktop/CC/PP/01-Convert/prueba.xlsx';
 
 const generarJSONDesdeExcel = (excelFilePath, jsonFilePath) => {
     // Cargo el archivo excel
     const workbook = xlsx.readFile(excelFilePath, { cellText: false });
-    const sheetName = workbook.SheetNames[0]; //Indicando que solo hay una hoja en el excel
+    const sheetName = workbook.SheetNames[0]; // Indicando que solo hay una hoja en el excel
 
     // Obtengo los datos del excel
     const sheetData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
 
-    //Funcion para que al tomar un dato en TRUE no lo ponga entre comillas;
+    // Función para convertir el valor booleano desde una cadena
     const getBooleanValue = (value) => {
         if (typeof value === 'string') {
             return value.toLowerCase() === 'true';
@@ -23,19 +18,22 @@ const generarJSONDesdeExcel = (excelFilePath, jsonFilePath) => {
         return Boolean(value);
     };
 
-
     // Mapeo los datos con la estructura requerida
     const jsonData = sheetData.map(row => {
-        const fechaAlta = moment().format('YYYY-MM-DD'); //modifico el campo para que tome la fecha actual
-        const mappedRow = {
-            "Codigo": String(row.Codigo), 
+        const fechaAlta = moment().format('YYYY-MM-DD');
+
+        // Convertir el valor del campo 'condicionesVenta_codigo' a string
+        const codigoCondicionVenta = String(row.condicionesVenta_codigo);
+
+        return {
+            "Codigo": String(row.Codigo),
             "razonSocial": row.razonSocial,
             "nombreFantasia": row.nombreFantasia,
             "email": row.email,
             "paginaWeb": row.paginaWeb,
             "nuestroCodigoProve": row.nuestroCodigoProve === "null" ? null : row.nuestroCodigoProve,
             "administradaPor": row.administradaPor === "null" ? null : row.administradaPor,
-            "tratImpositivo": row.tratImpositivo.toString().padStart(3, '0'), // Convertir a cadena de texto y agregar ceros a la izquierda *NO FUNCIONA SI ES UN NUMERO SIN 0*
+            "tratImpositivo": row.tratImpositivo.toString().padStart(3, '0'),
             "numeroImpositivoTipo": String(row.numeroImpositivoTipo),
             "numeroImpositivo1": String(row.numeroImpositivo1),
             "numeroImpositivo2": row.numeroImpositivo2 === "null" ? null : String(row.numeroImpositivo2),
@@ -53,54 +51,55 @@ const generarJSONDesdeExcel = (excelFilePath, jsonFilePath) => {
             "departamento": row.departamento === "null" ? null : row.departamento,
             "tasaDepartamental": row.tasaDepartamental,
             "convenioMultilateral": row.convenioMultilateral === "true" ? true : false,
-            "tratImpositivoProv": row.tratImpositivoProv.toString().padStart(3, '0'), // Convertir a cadena de texto y agregar ceros a la izquierda *NO FUNCIONA SI ES UN NUMERO SIN 0*
+            "tratImpositivoProv": row.tratImpositivoProv.toString().padStart(3, '0'),
             "habilitadoHasta": row.habilitadoHasta === "null" ? null : row.habilitadoHasta,
             "fechareg": row.fechareg === "null" ? null : row.fechareg,
             "imagenes": row.imagen && row.tipo ? [{ "imagen": row.imagen, "tipo": row.tipo }] : [],
-            "rutas": row.rutas_codigo  ? [{ "codigo": row.rutas_codigo }] : [],
+            "rutas": row.rutas_codigo ? [{ "codigo": row.rutas_codigo }] : [],
             "items": row.items_Codigo && row.items_CodigoPropio ? [{ "Codigo": row.items_Codigo, "CodigoPropio": row.items_CodigoPropio }] : [],
             "exenciones": [],
             "ingresosBrutos": row.ingresosBrutos_Provincia && row.ingresosBrutos_Porcentaje ? [{ "Provincia": row.ingresosBrutos_Provincia, "Porcentaje": row.ingresosBrutos_Porcentaje }] : [],
             "provinciasAgenteRetencion": row.provinciasAgenteRetencion_Provincia ? [{ "Provincia": row.provinciasAgenteRetencion_Provincia }] : [],
             "condicionesVenta": row.condicionesVenta_codigo && row.condicionesVenta_porDefecto && row.condicionesVenta_listaEstandar
-                              && row.condicionesVenta_listaOferta && row.condicionesVenta_listaMinima  ?
-                             [{ "codigo": `"${row.condicionesVenta_codigo}"`, "porDefecto": row.condicionesVenta_porDefecto === "true" ? true : false,
-                            "listaEstandar": row.condicionesVenta_listaEstandar, "listaOferta": row.condicionesVenta_listaOferta,
-                            "listaMinima": row.condicionesVenta_listaMinima }] : [], 
+                                && row.condicionesVenta_listaOferta && row.condicionesVenta_listaMinima ? 
+                                [{
+                                    "codigo": codigoCondicionVenta, // Usamos la variable 'codigoCondicionVenta' convertida a string
+                                    "porDefecto": row.condicionesVenta_porDefecto === "true" ? true : false,
+                                    "listaEstandar": row.condicionesVenta_listaEstandar,
+                                    "listaOferta": row.condicionesVenta_listaOferta,
+                                    "listaMinima": row.condicionesVenta_listaMinima
+                                }] : [],
             "atributos": row.atributos_codigo && row.atributos_valor ? [{ "codigo": row.atributos_codigo, "valor": row.atributos_valor }] : [],
             "comprobantesSuspendidos": [],
             "cuentasCorrientes": [],
-            //Manejo información de la matriz DOMICILIOS:
-            "domicilios":row.domicilios_Descripcion || row.domicilios_Domicilio1 || row.domicilios_Domicilio2 || row.domicilios_CodigoPostal ||
-            row.domicilios_Localidad || row.domicilios_Provincia || row.domicilios_Pais || row.domicilios_Telefono || row.domicilios_Fax ||
-            row.domicilios_Gnl || row.domicilios_Observaciones || row.domicilios_nroOrden || row.domicilios_Transportista ||
-            row.domicilios_Principal !== null || row.domicilios_Habilitado !== null ?
-                [{ 
-                "Descripcion": row.domicilios_Descripcion || null, 
-                "Domicilio1": row.domicilios_Domicilio1 || null, 
-                "Domicilio2": row.domicilios_Domicilio2 || null, 
-                "CodigoPostal": row.domicilios_CodigoPostal || null, 
-                "Localidad": row.domicilios_Localidad || null, 
-                "Provincia": row.domicilios_Provincia || null, 
-                "Pais": row.domicilios_Pais !== "null" ? row.domicilios_Pais : null, 
-                "Telefono": row.domicilios_Telefono,
-                "Fax": row.domicilios_Fax !== "null" ? row.domicilios_Pais : null, 
-                "Gnl": row.domicilios_Gnl !== "null" ? row.domicilios_Pais : null, 
-                "Observaciones": row.domicilios_Observaciones !== "null" ? row.domicilios_Observaciones : null, 
-                "nroOrden": row.domicilios_nroOrden !== "null" ? row.domicilios_nroOrden : null,
-                "Transportista": row.domicilios_Transportista !== "null" ? row.domicilios_Transportista : null, 
-                "Principal": getBooleanValue(row.domicilios_Principal), 
-                "Habilitado": getBooleanValue(row.domicilios_Habilitado) }] : [],
-                
-                "empresas": row.empresa_Codigo  ? [{ "codigo": row.empresa_Codigo }] : [],
+            "domicilios": row.domicilios_Descripcion || row.domicilios_Domicilio1 || row.domicilios_Domicilio2 || row.domicilios_CodigoPostal ||
+                          row.domicilios_Localidad || row.domicilios_Provincia || row.domicilios_Pais || row.domicilios_Telefono || row.domicilios_Fax ||
+                          row.domicilios_Gnl || row.domicilios_Observaciones || row.domicilios_nroOrden || row.domicilios_Transportista ||
+                          row.domicilios_Principal !== null || row.domicilios_Habilitado !== null ?
+                          [{ 
+                              "Descripcion": row.domicilios_Descripcion || null, 
+                              "Domicilio1": row.domicilios_Domicilio1 || null, 
+                              "Domicilio2": row.domicilios_Domicilio2 || null, 
+                              "CodigoPostal": row.domicilios_CodigoPostal || null, 
+                              "Localidad": row.domicilios_Localidad || null, 
+                              "Provincia": row.domicilios_Provincia || null, 
+                              "Pais": row.domicilios_Pais !== "null" ? row.domicilios_Pais : null, 
+                              "Telefono": row.domicilios_Telefono,
+                              "Fax": row.domicilios_Fax !== "null" ? row.domicilios_Fax : null, 
+                              "Gnl": row.domicilios_Gnl !== "null" ? row.domicilios_Gnl : null, 
+                              "Observaciones": row.domicilios_Observaciones !== "null" ? row.domicilios_Observaciones : null, 
+                              "nroOrden": row.domicilios_nroOrden !== "null" ? row.domicilios_nroOrden : null,
+                              "Transportista": row.domicilios_Transportista !== "null" ? row.domicilios_Transportista : null, 
+                              "Principal": getBooleanValue(row.domicilios_Principal), 
+                              "Habilitado": getBooleanValue(row.domicilios_Habilitado) 
+                          }] : [],
+            "empresas": row.empresa_Codigo ? [{ "codigo": row.empresa_Codigo }] : []
         };
-        return mappedRow;
-});
+    });
 
-// Guardar el JSON en un archivo y muestro mensaje
-
-fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
-console.log('JSON generado exitosamente.');
+    // Guardar el JSON en un archivo y mostrar mensaje
+    fs.writeFileSync(jsonFilePath, JSON.stringify(jsonData, null, 2));
+    console.log('JSON generado exitosamente.');
 };
 
 module.exports = {
